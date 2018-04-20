43cf356299afb4db1aba76827de382543c1b6f42
Eduard Climov <klimovedik88@gmail.com>
2018-04-20 19:57:05 +0300

    Database and the queries fully reworked to satisfy the task

--- eclimov/Readme.md
+++ eclimov/Readme.md
@@ -2,84 +2,116 @@
 
 ## SQL queries
 
-### Insert student
-INSERT INTO students(name, email, birth_date, group_id)  
-VALUES('Climov Eduard', 'klimovedik88@gmail.com', '1995-07-11', 1)
+### Insert students
+<pre>
+INSERT INTO `students` (`name`, `email`, `birth_date`, `group_id`) VALUES
+('Climov Eduard', 'klimovedik88@gmail.com', '1995-07-11', 1),
+('Vasiliev Vasilii', 'vasea@mail.com', '1995-03-12', 2),
+('Nicolaev Nicolai', 'nicola@mail.com', '1995-04-13', 2);
+</pre>
 
 ### Select student's info
-SELECT  
-&nbsp;&nbsp;&nbsp;&nbsp;students.name,  
-&nbsp;&nbsp;&nbsp;&nbsp;students.email,  
-&nbsp;&nbsp;&nbsp;&nbsp;students.birth_date,  
-&nbsp;&nbsp;&nbsp;&nbsp;groups.name AS group_name  
-FROM students  
-INNER JOIN groups  
-&nbsp;&nbsp;&nbsp;&nbsp;ON students.group_id = groups.id
+<pre>
+SELECT
+    students.name,
+    students.email,
+    students.birth_date,
+    groups.name AS group_name
+FROM students
+INNER JOIN groups
+    ON students.group_id = groups.id
+</pre>
 
 ### Update student
-UPDATE students  
-SET students.birth_date = '1995-07-12'  
+<pre>
+UPDATE students
+SET students.birth_date = '1995-07-12'
 WHERE students.name = 'Climov Eduard'
+</pre>
 
 ### Select top 10 students with max average mark
-SELECT  
-&nbsp;&nbsp;&nbsp;&nbsp;students.name,  
-&nbsp;&nbsp;&nbsp;&nbsp;IFNULL(AVG(marks.mark_value),0) AS avg_mark  
-FROM students  
-LEFT JOIN marks  
-&nbsp;&nbsp;&nbsp;&nbsp;ON students.id = marks.student_id  
-GROUP BY students.id  
-ORDER BY avg_mark DESC  
+<pre>
+SELECT 
+    students.name,
+    IFNULL(AVG(students_presence.mark),0) AS avg_mark
+FROM students
+LEFT JOIN students_presence
+    ON students.id = students_presence.student_id
+GROUP BY students.id
+ORDER BY avg_mark DESC
 LIMIT 10
+</pre>
 
-### Select top students grouped by classes
-SELECT  
-&nbsp;&nbsp;&nbsp;&nbsp;classes.name AS class,  
-&nbsp;&nbsp;&nbsp;&nbsp;students.name AS student,  
-&nbsp;&nbsp;&nbsp;&nbsp;avg_marks.avg_mark  
-FROM (  
-&nbsp;&nbsp;&nbsp;&nbsp;SELECT  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MAX(avg_marks.avg_mark) AS max_avg_mark,  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;avg_marks.class_id  
-&nbsp;&nbsp;&nbsp;&nbsp;FROM (  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SELECT  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AVG(mark_value) avg_mark,  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;class_id,  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;student_id  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FROM marks  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GROUP BY  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;class_id,  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;student_id  
-&nbsp;&nbsp;&nbsp;&nbsp;)avg_marks  
-&nbsp;&nbsp;&nbsp;&nbsp;GROUP BY  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;avg_marks.class_id  
-) max_avg_marks  
-INNER JOIN (  
-&nbsp;&nbsp;&nbsp;&nbsp;SELECT  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AVG(mark_value) avg_mark,  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;class_id,  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;student_id  
-&nbsp;&nbsp;&nbsp;&nbsp;FROM marks  
-&nbsp;&nbsp;&nbsp;&nbsp;GROUP BY  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;class_id,  
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;student_id  
-) avg_marks  
-&nbsp;&nbsp;&nbsp;&nbsp;ON avg_marks.class_id = max_avg_marks.class_id  
-&nbsp;&nbsp;&nbsp;&nbsp;AND avg_marks.avg_mark = max_avg_marks.max_avg_mark  
-INNER JOIN classes  
-&nbsp;&nbsp;&nbsp;&nbsp;ON avg_marks.class_id = classes.id  
-INNER JOIN students  
-&nbsp;&nbsp;&nbsp;&nbsp;ON avg_marks.student_id = students.id
+### Select top students grouped by objects
+<pre>
+SELECT 
+    objects.name AS object_name,
+    IFNULL(max_avg_marks.max_avg_mark, 'N/A') AS avg_mark,
+    IFNULL(students.name, 'N/A') AS student
+FROM objects
+LEFT JOIN (
+    SELECT 
+        MAX(avg_marks_st_obj.avg_mark) AS max_avg_mark,
+        avg_marks_st_obj.object_id
+    FROM (
+        SELECT 
+            AVG(sp.mark) AS avg_mark,
+            co.object_id,
+            sp.student_id
+        FROM students_presence sp
+        LEFT JOIN ( 
+            SELECT
+                classes.id AS class_id,
+                classes.object_id
+            FROM classes
+            INNER JOIN objects
+                ON classes.object_id = objects.id
+        ) co /*classes & objects*/
+            ON co.class_id = sp.class_id
+        GROUP BY 
+            co.object_id,
+            sp.student_id
+    ) avg_marks_st_obj /*avg marks for each student of each object*/
+    GROUP BY avg_marks_st_obj.object_id
+) max_avg_marks
+    ON objects.id = max_avg_marks.object_id
+LEFT JOIN (
+    SELECT 
+        AVG(sp.mark) AS avg_mark,
+        co.object_id,
+        sp.student_id
+    FROM students_presence sp
+    LEFT JOIN ( 
+        SELECT
+            classes.id AS class_id,
+            classes.object_id
+        FROM classes
+        INNER JOIN objects
+            ON classes.object_id = objects.id
+    ) co /*classes & objects*/
+        ON co.class_id = sp.class_id
+    GROUP BY 
+        co.object_id,
+        sp.student_id
+) avg_marks_st_obj /*avg marks for each student of each object*/
+    ON max_avg_marks.object_id = avg_marks_st_obj.object_id
+    AND max_avg_marks.max_avg_mark = avg_marks_st_obj.avg_mark
+LEFT JOIN students
+    ON avg_marks_st_obj.student_id = students.id
+</pre>
 
 ### Select list of professors for each student
-SELECT  
-&nbsp;&nbsp;&nbsp;&nbsp;students.name AS student,  
-&nbsp;&nbsp;&nbsp;&nbsp;GROUP_CONCAT(DISTINCT professors.name) AS professors  
-FROM marks  
-INNER JOIN students  
-&nbsp;&nbsp;&nbsp;&nbsp;ON marks.student_id = students.id  
-INNER JOIN classes  
-&nbsp;&nbsp;&nbsp;&nbsp;ON marks.class_id = classes.id  
-INNER JOIN professors  
-&nbsp;&nbsp;&nbsp;&nbsp;ON classes.professor_id = professors.id  
-GROUP BY marks.student_id
+<pre>
+SELECT
+    students.name AS student,
+    GROUP_CONCAT(DISTINCT professors.name) AS professors
+FROM
+    classes
+INNER JOIN professors
+    ON classes.professor_id = professors.id
+LEFT JOIN students_presence
+    ON classes.id = students_presence.class_id
+INNER JOIN students
+    ON students_presence.student_id = students.id
+GROUP BY students.id
+</pre>
--- eclimov/dump.sql
+++ eclimov/dump.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
 --
--- Host: localhost    Database: university
+-- Host: localhost    Database: university_v2
 -- ------------------------------------------------------
--- Server version	5.7.21-0ubuntu0.16.04.1
+-- Server version	5.7.21-0ubuntu0.17.10.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,11 +24,16 @@ DROP TABLE IF EXISTS `classes`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `classes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `object_id` int(11) NOT NULL,
   `professor_id` int(11) NOT NULL,
-  `when_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `is_exam` tinyint(1) NOT NULL DEFAULT '0',
+  `when_started` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `object_update` (`object_id`),
+  KEY `professor_update` (`professor_id`),
+  CONSTRAINT `object_update` FOREIGN KEY (`object_id`) REFERENCES `objects` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `professor_update` FOREIGN KEY (`professor_id`) REFERENCES `professors` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +42,7 @@ CREATE TABLE `classes` (
 
 LOCK TABLES `classes` WRITE;
 /*!40000 ALTER TABLE `classes` DISABLE KEYS */;
-INSERT INTO `classes` VALUES (1,'Etica',2,'2018-04-12 13:21:29'),(2,'Programare Web',1,'2018-04-12 13:21:40'),(3,'Baze de Date',3,'2018-04-13 22:39:30');
+INSERT INTO `classes` VALUES (1,1,2,0,'2018-04-19 22:59:37'),(2,3,1,0,'2018-04-19 22:59:37'),(3,1,3,0,'2018-04-19 22:59:37'),(4,2,4,0,'2018-04-19 22:59:37'),(5,2,4,1,'2018-04-20 13:30:37');
 /*!40000 ALTER TABLE `classes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,7 +55,7 @@ DROP TABLE IF EXISTS `faculties`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `faculties` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(200) NOT NULL,
   `when_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQUE` (`name`)
@@ -63,7 +68,7 @@ CREATE TABLE `faculties` (
 
 LOCK TABLES `faculties` WRITE;
 /*!40000 ALTER TABLE `faculties` DISABLE KEYS */;
-INSERT INTO `faculties` VALUES (1,'CIM','2018-04-12 13:14:40'),(2,'TA','2018-04-12 13:16:13');
+INSERT INTO `faculties` VALUES (1,'CIM','2018-04-19 22:55:12'),(2,'TA','2018-04-19 22:55:12');
 /*!40000 ALTER TABLE `faculties` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,7 +85,9 @@ CREATE TABLE `groups` (
   `faculty_id` int(11) NOT NULL,
   `when_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique` (`name`)
+  UNIQUE KEY `UNIQUE` (`name`),
+  KEY `faculty_delete` (`faculty_id`),
+  CONSTRAINT `faculty_delete` FOREIGN KEY (`faculty_id`) REFERENCES `faculties` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -90,38 +97,34 @@ CREATE TABLE `groups` (
 
 LOCK TABLES `groups` WRITE;
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES (1,'IA-142',1,'2018-04-12 13:17:50'),(2,'MN-134',1,'2018-04-12 13:18:04'),(3,'ATA-112',2,'2018-04-12 13:18:16');
+INSERT INTO `groups` VALUES (1,'IA-142',1,'2018-04-19 22:54:12'),(2,'MN-134',1,'2018-04-19 22:54:12'),(3,'ATA-112',2,'2018-04-19 22:54:12');
 /*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `marks`
+-- Table structure for table `objects`
 --
 
-DROP TABLE IF EXISTS `marks`;
+DROP TABLE IF EXISTS `objects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `marks` (
+CREATE TABLE `objects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mark_value` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `is_exam` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(200) NOT NULL,
   `when_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `student_id_index` (`student_id`) USING BTREE,
-  CONSTRAINT `student_delete` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `marks`
+-- Dumping data for table `objects`
 --
 
-LOCK TABLES `marks` WRITE;
-/*!40000 ALTER TABLE `marks` DISABLE KEYS */;
-INSERT INTO `marks` VALUES (1,9,1,1,0,'2018-04-12 13:22:34'),(2,8,2,1,0,'2018-04-12 13:22:51'),(3,8,1,1,1,'2018-04-12 13:23:09'),(4,8,3,2,0,'2018-04-12 13:23:21'),(5,7,3,1,0,'2018-04-13 22:12:58');
-/*!40000 ALTER TABLE `marks` ENABLE KEYS */;
+LOCK TABLES `objects` WRITE;
+/*!40000 ALTER TABLE `objects` DISABLE KEYS */;
+INSERT INTO `objects` VALUES (1,'Etica','2018-04-19 22:56:43'),(2,'Programare Web','2018-04-19 22:56:43'),(3,'Baze de Date','2018-04-19 22:56:43');
+/*!40000 ALTER TABLE `objects` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -133,9 +136,9 @@ DROP TABLE IF EXISTS `professors`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `professors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT '1',
-  `when_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(200) NOT NULL,
+  `when_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `when_retired` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -146,7 +149,7 @@ CREATE TABLE `professors` (
 
 LOCK TABLES `professors` WRITE;
 /*!40000 ALTER TABLE `professors` DISABLE KEYS */;
-INSERT INTO `professors` VALUES (1,'Ivanov Ivan',1,'2018-04-12 13:16:34'),(2,'Marinova Maria',1,'2018-04-12 13:16:47'),(3,'Konstantinov Konstantin',1,'2018-04-12 13:17:03'),(4,'Petrov Piotr',1,'2018-04-12 13:17:16');
+INSERT INTO `professors` VALUES (1,'Ivanov Ivan','2018-04-19 22:52:42',NULL),(2,'Marinova Maria','2018-04-19 22:52:42',NULL),(3,'Konstantinov Konstantin','2018-04-19 22:52:42',NULL),(4,'Petrov Piotr','2018-04-19 22:52:42',NULL);
 /*!40000 ALTER TABLE `professors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,13 +162,15 @@ DROP TABLE IF EXISTS `students`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `students` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
   `birth_date` date NOT NULL,
   `group_id` int(11) NOT NULL,
   `when_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique` (`email`)
+  UNIQUE KEY `UNIQUE` (`email`),
+  KEY `group_delete` (`group_id`),
+  CONSTRAINT `group_delete` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -178,6 +183,32 @@ LOCK TABLES `students` WRITE;
 INSERT INTO `students` VALUES (1,'Climov Eduard','klimovedik88@gmail.com','1995-07-11',1,'2018-04-12 13:14:22'),(2,'Vasiliev Vasilii','vasea@mail.com','1995-03-12',2,'2018-04-12 13:18:54'),(3,'Nicolaev Nicolai','nicola@mail.com','1995-04-13',2,'2018-04-12 17:01:26');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `students_presence`
+--
+
+DROP TABLE IF EXISTS `students_presence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students_presence` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `mark` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students_presence`
+--
+
+LOCK TABLES `students_presence` WRITE;
+/*!40000 ALTER TABLE `students_presence` DISABLE KEYS */;
+INSERT INTO `students_presence` VALUES (1,1,2,NULL),(2,2,4,NULL),(3,1,3,8),(4,3,1,9),(5,4,2,6),(6,4,1,7);
+/*!40000 ALTER TABLE `students_presence` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -188,4 +219,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-13 23:00:04
+-- Dump completed on 2018-04-20 19:52:38
